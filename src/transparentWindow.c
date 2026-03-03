baad3182e2f1935370fce8bf7cec3e3bdd5b4a04
Erdem Karaahmet <erdemkaraahmet@hotmail.com>
2026-03-03 18:19:38 +0300

    Inıtial commit, deps/SDL , .gitignore, sdl3 example, cmakelist added

--- src/transparentWindow.c
+++ src/transparentWindow.c
@@ -1,69 +1,74 @@
 // Source - https://stackoverflow.com/a/51956224
-// Posted by Stevoisiak, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-02-12, License - CC BY-SA 4.0
 
-// SDL window with transparent background v1.2
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_syswm.h>
+// SDL window with transparent background
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 #include <Windows.h>
 #include <stdbool.h>
 
 // Makes a window transparent by setting a transparency color.
 bool MakeWindowTransparent(SDL_Window* window, COLORREF colorKey) {
-    // Get window handle (https://stackoverflow.com/a/24118145/3357935)
-    SDL_SysWMinfo wmInfo;
-    SDL_VERSION(&wmInfo.version);  // Initialize wmInfo
-    SDL_GetWindowWMInfo(window, &wmInfo);
-    HWND hWnd = wmInfo.info.win.window;
+    HWND hWnd = (HWND)SDL_GetPointerProperty(
+        SDL_GetWindowProperties(window),
+        SDL_PROP_WINDOW_WIN32_HWND_POINTER,
+        NULL
+    );
 
-    // Change window type to layered (https://stackoverflow.com/a/3970218/3357935)
+    if (!hWnd) return false;
+
     SetWindowLong(hWnd, GWL_EXSTYLE, GetWindowLong(hWnd, GWL_EXSTYLE) | WS_EX_LAYERED);
-
-    // Set transparency color
     return SetLayeredWindowAttributes(hWnd, colorKey, 0, LWA_COLORKEY);
 }
 
 int main(int argc, char** argv) {
-    // Get resolution of primary monitor
-    int desktopWidth = GetSystemMetrics(SM_CXSCREEN);
-    int desktopHeight = GetSystemMetrics(SM_CYSCREEN);
+    SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window* window = SDL_CreateWindow("SDL Transparent Window",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        desktopWidth, desktopHeight, SDL_WINDOW_BORDERLESS);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    // Get resolution of primary monitor
+    SDL_DisplayID display = SDL_GetPrimaryDisplay();
+    const SDL_DisplayMode *mode = SDL_GetCurrentDisplayMode(display);
+    int desktopWidth  = mode->w;
+    int desktopHeight = mode->h;
+
+    SDL_Window* window = SDL_CreateWindow(
+        "SDL Transparent Window",
+        desktopWidth/3, desktopHeight/3,
+        SDL_WINDOW_BORDERLESS
+    );
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
 
     // Set background color to magenta and clear screen
-    SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+    //SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
     SDL_RenderClear(renderer);
 
     // Draw blue square in top-left corner
-    SDL_Rect rect1 = {0, 0, 100, 100};
+    SDL_FRect rect1 = {0, 0, 100, 100};
     SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
     SDL_RenderFillRect(renderer, &rect1);
 
     // Draw red square in center of the screen
-    SDL_Rect rect2 = {(desktopWidth-100)/2, (desktopHeight-100)/2, 100, 100};
+    SDL_FRect rect2 = {(desktopWidth-100)/2.0f, (desktopHeight-100)/2.0f, 100, 100};
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
     SDL_RenderFillRect(renderer, &rect2);
 
     // Add window transparency (Magenta will be see-through)
     MakeWindowTransparent(window, RGB(255, 0, 255));
 
-    // Render the square to the screen
+    // Render to screen
     SDL_RenderPresent(renderer);
 
     // Loop until user quits
     bool quit = false;
     SDL_Event event;
     while (!quit) {
-       while (SDL_PollEvent(&event) != 0) {
-           if (event.type == SDL_QUIT) {
-               quit = true;
-           }
-       }
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                quit = true;
+            }
+        }
     }
 
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
