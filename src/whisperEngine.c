f924b0108a2c67b3c39e4dd221c3709b73f4604a
Erdem Karaahmet <erdemkaraahmet@hotmail.com>
2026-04-06 05:53:18 +0300

    feat: implement whisper transcription engine
    - Initialize whisper context from model path
    - Add whisperProcess() with greedy sampling
    - Set thread count dynamically via SDL_GetNumLogicalCPUCores, currently no gpu support

--- src/whisperEngine.c
+++ src/whisperEngine.c
@@ -2,21 +2,46 @@
 #include "whisper.h"
 #include <stdio.h>
 #include <string.h>
+#include <SDL3/SDL.h>
 
 static struct whisper_context* ctx = NULL;
 
 // Initialize
 bool whisperInit(const char* modelPath) {
-
+    struct whisper_context_params cparams = whisper_context_default_params();
+    //cparams.use_gpu = true;
+    ctx = whisper_init_from_file_with_params(modelPath, cparams);
+    return (ctx != NULL);
 }
 
 // Returns true if there is new text
 bool whisperProcess(float* pcmf32, int n_samples, char* outputText, int outputLength) {
+    if (!ctx) return false;
 
+    struct whisper_full_params wparams = whisper_full_default_params(WHISPER_SAMPLING_GREEDY);
+    wparams.print_progress = false;
+    wparams.language = "en";
+    wparams.n_threads = SDL_GetNumLogicalCPUCores(); 
+
+    if (whisper_full(ctx, wparams, pcmf32, n_samples) != 0) {
+        return false;
+    }
+
+    const int n_segments = whisper_full_n_segments(ctx);
+    printf("segments: %d\n", n_segments);  // add this
+    for (int i = 0; i < n_segments; ++i) {
+        const char * text = whisper_full_get_segment_text(ctx, i);
+        strncat(outputText, text, outputLength);
+    }
+
+    return true;
 }
 
 void whisperFree() {
-
+    if (ctx != NULL) {
+        whisper_free(ctx);
+        ctx = NULL;
+    }
 }
 
 // Example from whisper github repo
