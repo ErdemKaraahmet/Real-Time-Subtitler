bbda17a0dd329245afa4117bfdf2bbecb0d4a5d0
lmmhhh-607 <lmm_hhh@icloud.com>
2026-07-26 20:56:58 +0800

    bug fixed

--- include/whisperEngine.h
+++ include/whisperEngine.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <stdbool.h>
 
-bool whisperInit(const char *modelPath, bool *use_gpu);
-bool whisperProcess(float *pcmf32, int n_samples, char *outputText, int outputLength, SubtitleToken* outputTokens, int *ontputTokenNums);
-void whisperFree(void);
 typedef struct
 {
     char text[64];
     float probability;
 } SubtitleToken;
+
+bool whisperInit(const char *modelPath, bool *use_gpu);
+bool whisperProcess(float *pcmf32, int n_samples, char *outputText, int outputLength, SubtitleToken* outputTokens, int *ontputTokenNums);
+void whisperFree(void);
